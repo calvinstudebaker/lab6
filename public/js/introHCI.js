22e5730c92591d5c9359ec221c661f76145aefcb
Calvin Studebaker <calstud@stanford.edu>
2014-02-13 03:36:44 +0000

    finished lab 6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,18 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	var url = "/project/" + idNumber;
+	$.get(url, addProject); 
+
 	console.log("User clicked on project " + idNumber);
+	console.log("Get call to: " + url);
+}
+
+function addProject(response){
+	console.log(response);
+	var id = response.id;
+	var html = "<img src='" + response.image + "' class='detailsImage'><h4>" + response.date + "</h4><p>" + response.summary + "</p>";
+	$("#project" + id + " .details").html(html);
 }
 
 /*
@@ -34,5 +45,15 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("/palette", respondToColor);
 	console.log("User clicked on color button");
+}
+
+function respondToColor(response){
+	var colors = response.colors['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
